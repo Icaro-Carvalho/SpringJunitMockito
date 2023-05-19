1bfff4dadecca035cb8703ef76e308d3ab5c0b15
Icaro-Carvalho <icaropc16@gmail.com>
2023-05-19 12:30:01 -0300

    Created the DTO pattern and implemented the ModelMapper

---
+++ src/main/java/br/com/icaro/api/domain/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package br.com.icaro.api.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+	
+	private Integer id;
+	private String name;
+	private String email;
+	@JsonIgnore
+	private String password;
+	
+	
+
+}
