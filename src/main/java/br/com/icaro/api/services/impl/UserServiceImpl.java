e3af328154cce1a1812a7dffd6ccd9d8a721afcb
Icaro-Carvalho <icaropc16@gmail.com>
2023-05-19 12:29:31 -0300

    Created the DTO pattern and implemented the ModelMapper

--- src/main/java/br/com/icaro/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/icaro/api/services/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import br.com.icaro.api.domain.User;
 import br.com.icaro.api.repositories.UserRepository;
 import br.com.icaro.api.services.UserService;
-import br.com.icaro.api.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,7 +18,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User findById(Integer id) {
 		Optional<User> userId = userRepository.findById(id);
-		return userId.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return userId.orElse(null);
 	}
 
 }
