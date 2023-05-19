f25836731d2b96ab4445c1d891cf65148a440cc9
Icaro-Carvalho <icaropc16@gmail.com>
2023-05-19 16:19:08 -0300

    Implementating the create user and atualizating UserDTO

--- src/main/java/br/com/icaro/api/services/UserService.java
+++ src/main/java/br/com/icaro/api/services/UserService.java
@@ -3,10 +3,13 @@ package br.com.icaro.api.services;
 import java.util.List;
 
 import br.com.icaro.api.domain.User;
+import br.com.icaro.api.domain.dto.UserDTO;
 
 public interface UserService {
 
 	User findById(Integer id);
 	
 	List<User> findAll();
+	
+	User create(UserDTO userDTO);
 }
--- src/main/java/br/com/icaro/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/icaro/api/services/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package br.com.icaro.api.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.icaro.api.domain.User;
+import br.com.icaro.api.domain.dto.UserDTO;
 import br.com.icaro.api.repositories.UserRepository;
 import br.com.icaro.api.services.UserService;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
 	public User findById(Integer id) {
 		Optional<User> userId = userRepository.findById(id);
@@ -24,6 +29,11 @@ public class UserServiceImpl implements UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	@Override
+	public User create(UserDTO userDTO) {
+		return userRepository.save(mapper.map(userDTO, User.class));
 	}
 
 }
